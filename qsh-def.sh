6d2e8a2cf5dd01b44ab96ffde42b39756b6c7169
Kurta László <kurta.laszlo@telekom.hu>
2011-10-06 00:44:23 +0200

    working version

--- qsh-def.sh
+++ qsh-def.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 qsh()  {
-  ssh -X -t -o ForwardAgent=yes $@ "/bin/bash"
+  echo "`date` $@" >> ~/.qsh_history
+  HOST=$1
+  scp -q $THISFILE $HOST:/tmp/$$qsh.sh
+  if [ $# -gt 1 ]; then
+    shift
+    PPAR="-c \"$@\""
+  else
+    PPAR="";
+  fi
+  ssh -X -t -o ForwardAgent=yes $HOST "source /tmp/$$qsh.sh; env THISFILE=\"/tmp/$$qsh.sh\" bash $PPAR"
+  ssh $HOST rm /tmp/$$qsh.sh
 }
 qshr()  {
-  ssh -X -t -o ForwardAgent=yes $@ "env SSH_AUTH_SOCK=\$SSH_AUTH_SOCK sudo su"
+  ssh -X -t -o ForwardAgent=yes $@ "sudo su root -c \"env SSH_AUTH_SOCK=\$SSH_AUTH_SOCK bash\""
 }
+ss() {
+  echo "Number of parameters: $#"
+  echo "Parameters value: $@"
+  if [ $# -gt 0 ]; then
+    PPAR='-c '"\"$@\""
+    eval "sudo env SSH_AUTH_SOCK=$SSH_AUTH_SOCK /bin/bash $PPAR"
+  else
+    PPAR="";
+    sudo env SSH_AUTH_SOCK=$SSH_AUTH_SOCK /bin/bash $PPAR
+  fi
+}
+export -f ss
+export -f qshr
 export -f qsh
