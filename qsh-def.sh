a1a9492d40fb51bede7d055a0ce0b5be3e6ae52e
Kurta László <kurta.laszlo@telekom.hu>
2012-01-17 15:00:57 +0100

    sm() X forwarding

--- qsh-def.sh
+++ qsh-def.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+program_name="qsh"
+
 s()  {
   echo "`date` $@" >> ~/.qsh_history
   HOST=$1
@@ -23,7 +26,7 @@ s()  {
     # do the job
     ssh -X -t -o ForwardAgent=yes $HOST "source /tmp/$$qsh.sh; env THISFILE=\"/tmp/$$qsh.sh\" $QENV bash $PPAR"
     # destroy the remote instance
-    ssh $HOST rm $RINST
+    ssh $HOST rm -f $RINST
   else
     echo "Public key authentication does not work properly, public key installation required."
     if [ -n "$QKEY" ]; then
@@ -56,13 +59,14 @@ s()  {
       # do the job
       ssh -X -t -o ForwardAgent=yes $HOST "source /tmp/$$qsh.sh; env THISFILE=\"/tmp/$$qsh.sh\" bash $PPAR"
       # destroy the remote instance
-      ssh $HOST rm $RINST
+      ssh $HOST rm -f $RINST
     else
       echo "Key installation failed."
     fi  
   fi
 }
 export -f s
+
 ss() {
   BPID=$$
   rm -f /tmp/q$BPID
@@ -95,30 +99,35 @@ sm() {
     quser=$1
     shift
     BPID=$$
-    rm -f /tmp/q$BPID
+    sudo rm -f /tmp/q$BPID
     TDISPLAY=$DISPLAY
     XATH="$HOME/.Xauthority"
+    sudo rm -f /tmp/.Xauthority.t
     cp $XATH /tmp/.Xauthority.t
-    chmod 644 /tmp/.Xauthority.t
+    sudo chown $quser:$quser /tmp/.Xauthority.t
+    sudo chmod 644 /tmp/.Xauthority.t
+    sudo chown $quser $THISFILE
     echo "export DISPLAY='$TDISPLAY'" >> /tmp/q$BPID
     echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> /tmp/q$BPID
-    echo "export XAUTHORITY=/home/$quser/.Xauthority" >> /tmp/q$BPID
+    echo "export XAUTHORITY=\$HOME/.Xauthority" >> /tmp/q$BPID
     echo "xauth merge /tmp/.Xauthority.t" >> /tmp/q$BPID
     echo "export THISFILE='$THISFILE'" >> /tmp/q$BPID
     echo "export QKEY='$QKEY'" >> /tmp/q$BPID
     echo "source \$THISFILE" >> /tmp/q$BPID
+    echo "rm -f \$THISFILE" >> /tmp/q$BPID
 
     if [ $# -gt 0 ]; then
       echo "bash -c \"$@\"" >> /tmp/q$BPID
     else
       echo "bash -i" >> /tmp/q$BPID
     fi
-    sudo -u $quser /bin/bash /tmp/q$BPID
-    rm -f /tmp/q$BPID
-    rm -f /tmp/.Xauthority.t
+    sudo -H -u $quser /bin/bash /tmp/q$BPID
+    sudo rm -f /tmp/q$BPID
+    sudo rm -f /tmp/.Xauthority.t
   fi
 }
 export -f sm
+
 sqsh() {
   HOST=$1
   shift
@@ -126,19 +135,43 @@ sqsh() {
   screen -t $HOST bash -c "export THISFILE=$THISFILE && qsh $HOST $maradek" 
 }
 export -f sqsh 
+
 qshh() {
   grep "$1" ~/.qsh_history
 }
 export -f qshh
-sr() {
+
+sreload() {
   source $THISFILE
 }
-export -f sr
+export -f sreload
+
+se() {
+  vim $THISFILE
+}
+export -f se
+
 h() {
   history | grep $1
 }
 export -f h
+
 hh() {
   cat ~/.qsh_history | grep $1
 }
 export -f hh
+
+
+shelp() {
+  echo "Usage:
+    s <hostname> [command(s)]
+    ss
+    sm <username>
+    h <word> : search in bash history
+    hh <word> : search in local ${program_name} history
+    sreload : Reload the definition script
+    se : Edit definition script
+
+  "
+}
+export -f shelp
